50253f7dbcc78b229690484af9849bdd29107e5e
Jaskaran Singh Lamba <jaskaran@jaskaran-desktop.(none)>
2013-03-19 21:01:35 +0530

    first

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<fstream>
+#include<stdio.h>
+using namespace std;
+
+class emp
+                {
+                    char name[25],gender,fnam[25];
+                    int eno;
+                    float basic;
+
+                    public:
+
+                    void input()
+                    {
+                        cout<<"\nName:";
+                        gets(name);
+
+                        cout<<"Gender :";
+                        cin>>gender;
+
+                       cout<<"Father's name :";
+                       cin>>fnam;
+
+                        cout<<"Enter the employee number assigned :";
+                        cin>>eno;
+
+                        cout<<"Basic Pay :";
+                        cin>>basic;
+
+                   }
+
+                    void output()
+                    {
+                        cout<<"Name :";
+                        puts(name);
+
+                        cout<<"Gender :";
+                        cout<<gender;
+
+                        cout<<"\nFather's name :";
+                        puts(fnam);
+
+                        cout<<"Employee number :";
+                        cout<<eno;
+
+                        cout<<"\nBasic Pay :";
+                        cout<<basic<<endl<<endl;
+
+                   }
+
+                };
+
+        int main()
+        {
+            emp e;
+            cout<<"\nEnter details of the employee\n\n";
+            e.input();
+
+            cout << "\x1b[2J\x1b[1;1H" << flush;
+
+            cout<<"\nDetails entered are \n\n";
+            e.output();
+
+            return 0;
+
+        }
+
+
+
